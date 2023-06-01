2596c1a3a199fbead9d09c07c8277676761da92e
NixOS <me@meiro.top>
2023-06-01 18:53:28 +0800

    remove eww

--- home/desktop/bars.nix
+++ home/desktop/bars.nix
@@ -1,11 +1,4 @@
 {pkgs, ...}: {
-  programs = {
-    eww = {
-      enable = true;
-      package = pkgs.eww-wayland;
-      configDir = ./.eww;
-    };
-  };
   home.packages = with pkgs; [
     # mypkgs.shox
     mypkgs.baru
