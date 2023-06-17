3c19e5152919ced6d75e7b59201bf523853e07e9
/nix/store/7p4zri1fq434nnv11qmgx5n2n84v3x05-python3-3.10.11-env/bin/python ~/scripts/random_pokemon_name.py) <konecho@outlook.com>
2023-06-17 22:00:20 +0800

    qtile failed

--- home/desktop/qtile/default.nix
+++ home/desktop/qtile/default.nix
@@ -2,26 +2,51 @@
   pkgs,
   config,
   ...
-}: {
-  home.packages = with pkgs; [
+}:
+with pkgs; let
+  qtile-package = qtile.unwrapped;
+  extra-packages = ps:
+    with ps;
+      [qtile-package]
+      ++ [
+        python-lsp-server
+        autopep8
+        black
+        mypy
+        requests
+        qtile-extras
+      ];
+  pyEnv = python3.withPackages extra-packages;
+  cfgText = ''
+    ${builtins.readFile ./config.py}
+    # start the applications at Qtile startup
+    @hook.subscribe.startup
+    def startup():
+        exec_once("${pkgs.swww}/bin/swww init &")
+        exec_once("${pkgs.swww}/bin/swww img ${config.stylix.image} &")
+        # exec_once("${pkgs.mypkgs.pokemon-terminal}/bin/pokemon -w 615 &")
+        exec_once("${pkgs.clash-verge}/bin/clash-verge &")
+  '';
+  cfgFile = writeScript "config.py" cfgText;
+  # cfgFile = fetchurl {
+  #   url = "https://raw.githubusercontent.com/qtile/qtile/master/libqtile/resources/default_config.py";
+  #   sha256 = "sha256-R6ZzYq28g6lDXlnGCq5SMhuaQ+XkvJcZ9BP5qtpduBM=";
+  # };
+in {
+  home.packages = [
     (
-      let
-        python-packages = python-packages:
-          with python-packages; [
-            python-lsp-server
-            autopep8
-            black
-            requests
-
-            qtile
-            qtile-extras
-          ];
-        pyEnv = python3.withPackages python-packages;
-      in
-        writeShellScriptBin "qtile" ''
-          ${pyEnv}/bin/qtile start -b wayland \
-          --config ${./config.py}
-        ''
+      writeShellScriptBin "qtile-run" ''
+        ${pyEnv}/bin/qtile start -b wayland --config "${cfgFile}" & waitPID=\$!
+      ''
+    )
+    (
+      writeShellScriptBin "qtile-check" ''
+        ${pyEnv}/bin/qtile check --config ${cfgFile}
+      ''
     )
   ];
+  home.file.qtile-config = {
+    text = cfgText;
+    target = ".config/qtile/config.py";
+  };
 }
