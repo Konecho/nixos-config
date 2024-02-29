57824e652c809df5d07f44a07c2603bb121e6c85
雷吉艾斯 <konecho@outlook.com>
2024-02-29 09:25:30 +0800

    fix git config

--- home/commandline/pkm-shell.nix
+++ home/commandline/pkm-shell.nix
@@ -13,12 +13,26 @@
         fish_greeting = ''
           random_pokemon
           echo $POKEMON|awk -F',' '{print$4}'|tr "'" " "| ${pkgs.pokemonsay}/bin/pokemonsay -N -D $POKEMON_NATIONAL_DEX_NUMBER
+          config_local_git
+        '';
+        z = ''
+          ${pkgs.zoxide}/bin/__zoxide_z $argv
+          config_local_git    
         '';
         random_pokemon = ''
           # 898
           set -g POKEMON_NATIONAL_DEX_NUMBER $(shuf -n 1 -i 1-898)
           set -g POKEMON $(sed -n {$POKEMON_NATIONAL_DEX_NUMBER}p ${./pmlist.csv})
-          git config user.name "$(echo $POKEMON|awk -F',' '{print$2}')"
+          config_local_git
+        '';
+        config_local_git = ''
+          if [ $(git rev-parse --is-inside-work-tree) = 'true' ]
+              git config user.name "$(echo $POKEMON|awk -F',' '{print$2}')"
+          end           
+        '';
+        # global config is locked by nix
+        gitui = lib.mkBefore ''
+          config_local_git
         '';
       };
     };
