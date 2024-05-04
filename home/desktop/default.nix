184a25ede80671a801d3c1c1259f349dd82f5a16
拉帝亚斯 <konecho@outlook.com>
2024-05-04 12:50:46 +0800

    remove dwl qtile ags

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -11,35 +11,12 @@
     ./mako.nix
 
     # ./gnome.nix
-    # ./ags.nix
 
     # ./hyprland.nix
-    # ./dwl
-    # ./qtile
+
     ./niri.nix
     ./river
 
-    # ./bars.nix
-    # ./eww
     ./wl-utils.nix
   ];
-  # services.mako = {
-  #   enable = true;
-  #   # font = "monospace 12";
-  # };
-  # services.clipman.enable = true;
-  # programs.eww = {
-  #   enable = true;
-  #   package = pkgs.eww-wayland;
-  #   configDir = ./eww;
-  # };
-  # home.packages = with pkgs; [
-  #   ## [[app runner]]
-  #   # kickoff
-  #   wofi
-  #   #   lua
-  #   #   pamixer
-  #   #   brightnessctl
-  #   #   (nerdfonts.override {fonts = ["JetBrainsMono"];})
-  # ];
 }
