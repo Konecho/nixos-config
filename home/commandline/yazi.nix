6128a303fee2e43ce67d055c813b35e8e6467f5c
熔蚁兽 <konecho@outlook.com>
2025-02-15 21:22:20 +0800

    update yazi plugin

--- home/commandline/yazi.nix
+++ home/commandline/yazi.nix
@@ -3,7 +3,6 @@
     enable = true;
     enableZshIntegration = true;
     shellWrapperName = "lf";
-
     settings = {
       manager = {
         show_hidden = true;
@@ -25,13 +24,18 @@
         }
       ];
     };
-
     plugins = let
       yazi-plugins = pkgs.fetchFromGitHub {
         owner = "yazi-rs";
         repo = "plugins";
-        rev = "7afba3a73cdd69f346408b77ea5aac26fe09e551";
-        hash = "sha256-w9dSXW0NpgMOTnBlL/tzlNSCyRpZNT4XIcWZW5NlIUQ=";
+        rev = "beb586aed0d41e6fdec5bba7816337fdad905a33";
+        hash = "sha256-enIt79UvQnKJalBtzSEdUkjNHjNJuKUWC4L6QFb3Ou4=";
+      };
+      yazi-starship = pkgs.fetchFromGitHub {
+        owner = "Rolv-Apneseth";
+        repo = "starship.yazi";
+        rev = "d1cd0a38aa6a2c2e86e62a466f43e415f781031e";
+        sha256 = "sha256-XiEsykudwYmwSNDO41b5layP1DqVa89e6Emv9Qf0mz0=";
       };
     in {
       chmod = "${yazi-plugins}/chmod.yazi";
@@ -39,12 +43,7 @@
       max-preview = "${yazi-plugins}/max-preview.yazi";
       git = "${yazi-plugins}/git.yazi";
       smart-filter = "${yazi-plugins}/smart-filter.yazi";
-      starship = pkgs.fetchFromGitHub {
-        owner = "Rolv-Apneseth";
-        repo = "starship.yazi";
-        rev = "9c37d37099455a44343f4b491d56debf97435a0e";
-        sha256 = "sha256-wESy7lFWan/jTYgtKGQ3lfK69SnDZ+kDx4K1NfY4xf4=";
-      };
+      starship = yazi-starship;
     };
 
     initLua = ''
