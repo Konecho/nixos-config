ccbbc7b50fc1f02c9e5b2a352daa249f0ab7bf0a
小卡比兽 <konecho@outlook.com>
2025-04-21 14:53:31 +0800

    DoH

--- hosts/wsl/system.nix
+++ hosts/wsl/system.nix
@@ -47,6 +47,39 @@
   networking.proxy.default = "http://192.168.80.1:7890";
   services.udev.extraRules = ''
   '';
+  networking = {
+    nameservers = ["127.0.0.1" "::1"];
+    # If using dhcpcd:
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    # If using NetworkManager:
+    networkmanager.dns = "none";
+  };
+  # wsl
+  # wsl.wslConf.
+  wsl.wslConf.network.generateResolvConf = false;
+  # networking.resolvconf.useLocalResolver = true;
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    # Settings reference:
+    # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+      # Add this to test if dnscrypt-proxy is actually used to resolve DNS requests
+      # query_log.file = "/var/log/dnscrypt-proxy/query.log";
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     wsl-open
