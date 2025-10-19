ca4d866967ad8381f05141a6cc8d318cef2748fb
钳尾蝎 <konecho@outlook.com>
2025-10-19 16:13:30 +0800

    update mpvpaper

--- home/desktop/services.nix
+++ home/desktop/services.nix
@@ -1,49 +1,37 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }: {
   home.packages = with pkgs; [
-    # niri
     # cage
     # xwayland-run
-    # xwayland-satellite
     # gamescope
 
-    # activate-linux
-
     mpv
-    # mpvpaper
-
     lswt
   ];
   programs.fuzzel.enable = true;
   services.udiskie.enable = true;
-  services.udiskie.settings = {
-    icon_names.media = ["media-optical"];
-  };
   programs.niri.settings.spawn-at-startup = [
-    # {command = ["activate-linux" "-d"];}
     {command = ["fcitx5" "-r" "-d"];}
     {command = ["clash-verge"];}
   ];
-
-  gtk = {
-    enable = true;
-  };
+  gtk.enable = true;
   ## [[mpvpaper]]
   systemd.user.sessionVariables.PATH = "${pkgs.systemd}:${config.home.homeDirectory}/.nix-profile/bin:/run/current-system/sw/bin/";
-  # systemd.user.services.mpvpaper = {
-  #   Install = {WantedBy = ["sway-session.target"];};
-  #   Unit = {
-  #     Description = "wallpaper program that allows you to play videos with mpv as your wallpaper";
-  #     Documentation = ["man:mpvpaper(1)"];
-  #   };
-  #   Service = {
-  #     ExecStart = ''
-  #       ${pkgs.mpvpaper}/bin/mpvpaper -o "no-audio --loop-playlist shuffle input-ipc-server=/tmp/mpv-socket" \
-  #       HDMI-A-1 ${config.home.homeDirectory}/media/video/wallpapers
-  #     '';
-  #   };
-  # };
+  systemd.user.services.mpvpaper = lib.mkIf config.programs.mpvpaper.enable {
+    Install = {WantedBy = [config.wayland.systemd.target];};
+    Unit = {
+      Description = "wallpaper program that allows you to play videos with mpv as your wallpaper";
+      Documentation = ["man:mpvpaper(1)"];
+    };
+    Service = {
+      ExecStart = ''
+        ${lib.getExe pkgs.mpvpaper} -o "no-audio --loop-playlist shuffle input-ipc-server=/tmp/mpv-socket" \
+        HDMI-A-1 ${config.home.xdg.userDirs.videos}/wallpapers
+      '';
+    };
+  };
 }
