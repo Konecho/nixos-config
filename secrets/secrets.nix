22d1fa099970a994ba44edb8ec255e6a1e854fa9
mei <konecho@outlook.com>
2026-01-04 21:41:30 +0800

    add secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,12 @@
 let
   wsl-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6JF1QVNQH+DwPENPTDyi+XUJilipmU6dea1i8chd2d";
-  users = [wsl-user];
+  deskmini-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6z6TbfoDoVnjsUDgdYSLVQO15mWkFxsPc3i5Yw738H";
+  users = [wsl-user deskmini-user];
 
   wsl-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsWQUYB/KhKLag6J0C3hDwdNGOpqelzQ/gXt8jAG9pE";
   systems = [wsl-host];
+
+  all-secrets = users ++ systems;
 in {
-  # "openrouter.age".publicKeys = [wsl-user wsl-host];
+  "openrouter.age".publicKeys = all-secrets;
 }
